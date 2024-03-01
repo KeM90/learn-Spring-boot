660d6731a535f5342816a448f61494e943e20472
KeM90 <k.meier.09@gmx.de>
2024-03-01 12:13:54 +0100

    new SpringbootProject

---
+++ src/main/java/com/conka/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,23 @@
+package com.conka.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+    // /courses
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+            new Course(1, "Learn AWS", "KeM90"),
+            new Course(2, "Learn DevOps", "KeM90")
+
+        );
+    }
+
+
+}
