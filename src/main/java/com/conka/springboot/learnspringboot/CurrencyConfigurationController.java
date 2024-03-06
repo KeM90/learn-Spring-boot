8e2a7fd600e1329e77eb0180686f9f94105734cf
KeM90 <k.meier.09@gmx.de>
2024-03-06 11:50:43 +0100

    fix

--- src/main/java/com/conka/springboot/learnspringboot/CurrencyConfigurationController.java
+++ src/main/java/com/conka/springboot/learnspringboot/CurrencyConfigurationController.java
@@ -1,7 +1,5 @@
 package com.conka.springboot.learnspringboot;
 
-import java.util.Arrays;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
